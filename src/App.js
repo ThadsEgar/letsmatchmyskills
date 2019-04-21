c01e6ca85b2fe1a9998c4102d88442fc71693855
Thads Egar <tmichaelegar@gmail.com>
2019-04-20 22:39:42 -0500

    Init Form Block

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
+import { Button,  Form } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          Lets Match Your Skills
+        </div>
+
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+          <Form.Label>Enter your skills</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+          Test
+    </Form.Text>
+  </Form.Group>
+  </Form>
+
+  <Button variant="primary" size="lg" block>Primary</Button>
+
       </div>
     );
   }
