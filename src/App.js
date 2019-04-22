b94bc42f74b2e6958dac4131dc094286bfbc0630
Thads Egar <tmichaelegar@gmail.com>
2019-04-21 21:34:38 -0500

    Skill retriever onclick

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import React, { Component } from 'react';
-import { Button,  Form } from 'react-bootstrap';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Button, Form } from "react-bootstrap";
+import "./App.css";
 
-
+//global variables
+var skillsArray = [];
 
 class App extends Component {
+
+  enterSkill(){
+    skillsArray.push(document.getElementById("skillForm").value);
+    console.log(skillsArray.pop());
+  }
+
   render() {
     return (
       <div className="App">
-        <div>
-          Lets Match Your Skills
-        </div>
-
+        {/*JavaScript*/}
+        <div className="Welcome">Lets Match Your Skills</div>
+        <div>Skills List</div>
         <Form>
           <Form.Group controlId="formBasicEmail">
-          <Form.Label>Enter your skills</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-          Test
-    </Form.Text>
-  </Form.Group>
-  </Form>
-
-  <Button variant="primary" size="lg" block>Primary</Button>
-
+            <Form.Label>Enter your skills</Form.Label>
+            <Form.Control id="skillForm" placeholder="Enter skill" />
+          </Form.Group>
+        </Form>
+        <Button variant="primary" size="lg" onClick={this.enterSkill} block>
+          Add Skill
+        </Button>
+        <Button variant="primary" size="lg" block>
+          Lets Go!
+        </Button>
       </div>
     );
   }
---
+++ src/JobComponent.js
@@ -0,0 +1,13 @@
+import React, {Component from "react";
+import ReactDOM from "react-dom";
+
+class JobComponent extends Component{
+
+  render(){
+    return(
+
+    );
+  }
+}
+
+export default JobComponent;
